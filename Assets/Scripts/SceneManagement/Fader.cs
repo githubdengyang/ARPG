b176ef87068282c34faa8b1266d49a2243c8e13b
yang.deng <1341816304@qq.com>
2024-06-20 05:03:07 +0800

    场景管理

---
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -0,0 +1,52 @@
+using RPG.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+using Unity.Jobs;
+
+namespace RPG.SceneManagement
+{
+	public class Fader : MonoBehaviour
+	{
+		private CanvasGroup canvasGroup;
+
+
+		private void Start()
+		{
+			canvasGroup = GetComponent<CanvasGroup>();
+		}
+
+		public IEnumerator FadeOut(float time)
+		{
+			Debug.Log("Fading out");
+			while (true) 
+			{
+				canvasGroup.alpha += Time.deltaTime / time;
+				if (canvasGroup.alpha >= 1)
+				{
+					canvasGroup.alpha = 1;
+					yield break;
+				}
+				yield return null;
+			}
+		}
+
+		public IEnumerator FadeIn(float time)
+		{
+			Debug.Log("Fading in");
+			while (true)
+			{
+				canvasGroup.alpha -= Time.deltaTime / time;
+				if (canvasGroup.alpha <= 0)
+				{
+					canvasGroup.alpha = 0;
+					yield break;
+				}
+				yield return null;
+			}
+		}
+	}
+}
