f5483ddc7b79215a73422e3196661ceec068568f
yang.deng <1341816304@qq.com>
2024-08-05 14:55:42 +0800

    Quest 1

---
+++ Assets/Scripts/UI/Quests/QuestItemUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Quests;
+using TMPro;
+using UnityEngine;
+
+public class QuestItemUI : MonoBehaviour
+{
+	[SerializeField] TextMeshProUGUI title;
+	[SerializeField] TextMeshProUGUI progress;
+	Quest quest;
+	public void Setup(Quest quest)
+	{
+		this.quest = quest;
+		title.text = quest.GetTitle();
+		progress.text = "0/" + quest.GetObjectiveCount();
+	}
+
+	public Quest GetQuest()
+	{
+		return quest;
+	}
+}
---
+++ Assets/Scripts/UI/Quests/QuestTooltipSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameDevTV.Core.UI.Tooltips;
+using UnityEngine;
+using RPG.Quests;
+
+namespace RPG.UI.Quests
+{
+	public class QuestTooltipSpawner : TooltipSpawner
+	{
+		public override bool CanCreateTooltip()
+		{
+			return true;
+		}
+
+		public override void UpdateTooltip(GameObject tooltip)
+		{
+			Quest quest = GetComponent<QuestItemUI>().GetQuest();
+			tooltip.GetComponent<QuestTooltipUI>().Setup(quest);
+		}
+	}
+}
